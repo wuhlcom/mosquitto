0971818830da5243b1cada490030125d9e2ab9a9
wuhlcom <wuhlcom@qq.com>
2016-11-24 15:46:37 +0800

    sed mqtt.conf

--- bash_mqtt/mqttClient.sh
+++ bash_mqtt/mqttClient.sh
@@ -14,93 +14,127 @@ source $sPath/mqtt.conf
 source $sPath/tcpdump.sh
 
 mqttClient(){
-if $capFlag;then
- cap
-fi
- 
-j=0
-#sTime=`date +"%Y-%m-%d %H:%M:%S"`
-#start=`date +%s -d "$sTime"`
-for i in `seq $sNum $eNum`
-do	
-	topic="sendtopicpc166$i"
-	id="clientidpc166$i"
-        mosquitto_sub -t $topic -h $srv_ip -p $srv_port -q $j -i $id -k $keepLive&
-	echo client  \'$id\' sub topic \'$topic\'
-	j=`expr $j + 1`
-	if [ $j -ge 3 ]; then
-		j=0
+	if $capFlag;then
+	 cap
 	fi
-	echo `date +"%Y-%m-%d %H:%M:%S"`>"$sPath"/subLoopNum
-        echo `expr $i - $sNum + 1`>>"$sPath"/subLoopNum	
-done
-#eTime=`date +"%Y-%m-%d %H:%M:%S"`
-#end=`date +%s -d "$eTime"`
-
-#monitor_log subresult&
-$sPath/logger.sh monitorlog&
-
-while true 
-do
+ 
+	j=0
+	#sTime=`date +"%Y-%m-%d %H:%M:%S"`
+	#start=`date +%s -d "$sTime"`
 	for i in `seq $sNum $eNum`
-	do
+	do	
 		topic="sendtopicpc166$i"
-		id="pubidpc166$i"
-		msg="PC166testMSG$i"
-		mosquitto_pub -t $topic -m $msg -h $srv_ip -p $srv_port -i $id  -q 2 
-		sleep 1
+		id="clientidpc166$i"
+        	mosquitto_sub -t $topic -h $srv_ip -p $srv_port -q $j -i $id -k $keepLive&
+		echo client  \'$id\' sub topic \'$topic\'
+		j=`expr $j + 1`
+		if [ $j -ge 3 ]; then
+			j=0
+		fi
+		echo `date +"%Y-%m-%d %H:%M:%S"`>"$sPath"/subLoopNum
+	        echo `expr $i - $sNum + 1`>>"$sPath"/subLoopNum	
 	done
-done
+	#eTime=`date +"%Y-%m-%d %H:%M:%S"`
+	#end=`date +%s -d "$eTime"`
+
+	#monitor_log subresult&
+	$sPath/logger.sh monitorlog&
+
+	while true 
+	do
+		for i in `seq $sNum $eNum`
+		do
+			topic="sendtopicpc166$i"
+			id="pubidpc166$i"
+			msg="PC166testMSG$i"
+			mosquitto_pub -t $topic -m $msg -h $srv_ip -p $srv_port -i $id  -q 2 
+			sleep 1
+		done
+	done
 }
 
 #mosuqitto_sub
 mqtt_sub(){
-if $capFlag;then
- cap
-fi
-
-j=0
-for i in `seq $sSubNum $eSubNum`
-do	
-	topic="sendtopicpc166$i"
-	id="clientidpc166$i"
-        mosquitto_sub -t $topic -h $srv_ip -p $srv_port -q $j -i $id -k $keepLive&
-	echo client  \'$id\' sub topic \'$topic\'
-	j=`expr $j + 1`
-	if [ $j -ge 3 ]; then
-		j=0
+	if $capFlag;then
+	 cap
 	fi
-	echo `date +"%Y-%m-%d %H:%M:%S"`>"$sPath"/mqttSubNum
-        echo `expr $i - $sSubNum + 1`>>"$sPath"/mqttSubNum	
-done
 
-#monitor_log subresult&
-$sPath/logger.sh monitorlog&
+	j=0
+	for i in `seq $sSubNum $eSubNum`
+	do	
+		topic="sendtopicpc166$i"
+		id="clientidpc166$i"
+        	mosquitto_sub -t $topic -h $srv_ip -p $srv_port -q $j -i $id -k $keepLive&
+		echo client  \'$id\' sub topic \'$topic\'
+		j=`expr $j + 1`
+		if [ $j -ge 3 ]; then
+			j=0
+		fi
+		echo `date +"%Y-%m-%d %H:%M:%S"`>"$sPath"/mqttSubNum
+	        echo `expr $i - $sSubNum + 1`>>"$sPath"/mqttSubNum	
+	done
+
+	#monitor_log subresult&
+	$sPath/logger.sh monitorlog&
 
 }
 
 #mosquitto_pub
 mqtt_pub(){
-while true 
-do
-	for i in `seq $sPubNum $ePubNum`
+	while true 
 	do
-		topic="sendtopicpc166$i"
-		id="pubidpc166$i"
-		msg="PC166testMSG$i"
-		mosquitto_pub -t $topic -m $msg -h $srv_ip -p $srv_port -i $id  -q $pubQos 
-		sleep 1
+		for i in `seq $sPubNum $ePubNum`
+		do
+			topic="sendtopicpc166$i"
+			id="pubidpc166$i"
+			msg="PC166testMSG$i"
+			mosquitto_pub -t $topic -m $msg -h $srv_ip -p $srv_port -i $id  -q $pubQos 
+			sleep 1
+		done
 	done
-done
 }
 
-if [ $1 = "mqttsub" ];then
-  mqtt_sub
-elif [ $1 = "mqttpub" ];then
-  mqtt_pub
-elif [ $1 = "mqttclient" ];then
-  mqttClient
-else
- echo "Please input mqttsub or mqttpub or mqttclient"
-fi
-	
+#stop sub or pub process
+stopSubPub(){
+	  pkill mosquitto_sub
+	  pids=`ps -ef |grep mqttClient.sh|grep "\/bin\/bash"|awk -F " " '{print $2}'`
+	  OLD_IFS="$IFS"
+	  IFS=" "
+	  arr=($pids)
+          echo $arr
+	  IFS="$OLD_IFS"
+	  for pid in ${arr[@]};
+	  do 
+	    kill -9 $pid
+	  done
+}
+
+case $1 in
+   "mqttsub")
+     mqtt_sub
+     ;;
+   "mqttpub")
+     mqtt_pub
+     ;;
+   "mqttclient")
+     mqttClient
+     ;;
+   "stopsub")
+     stopSubPub
+     ;;
+   *)
+     echo "Please input mqttsub or mqttpub or mqttclient"
+     ;;
+esac
+
+#if [ $1 = "mqttsub" ];then
+#  mqtt_sub
+#elif [ $1 = "mqttpub" ];then
+#  mqtt_pub
+#elif [ $1 = "mqttclient" ];then
+#  mqttClient
+#elif [ $1 = "stopsub" ];then
+# stopSubPub
+#else
+# echo "Please input mqttsub or mqttpub or mqttclient"
+#fi
